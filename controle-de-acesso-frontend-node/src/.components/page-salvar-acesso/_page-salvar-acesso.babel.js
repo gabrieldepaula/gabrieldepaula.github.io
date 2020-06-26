0f1231cce7c18f37e1bb25093e77db9772dabcf5
Gabriel Leite <gabrielleite@luego.com.br>
2020-06-26 20:44:26 -0300

    Publicando no GitHub

---
+++ controle-de-acesso-frontend-node/src/.components/page-salvar-acesso/_page-salvar-acesso.babel.js
@@ -0,0 +1,108 @@
+/**
+ * Cada componente recebe as variáveis explicadas abaixo
+ *
+ * @param {HTMLElement} el - Elemento DOM que seleciona o componente em si
+ * @param {Object} params  - Parâmetros para serem utilizados
+ */
+
+const $el = $(el);
+const $form = $el.find('form');
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+
+if(id) {
+    $form.find('input').val('Carregando...');
+    $.get(base_api + 'morador/pesquisar', function(items) {
+        let $items = $(items);
+        $items.each(function(itemIndex, item) {
+            if(id == item.id) {
+                $el.find('.page-title').text('Editar morador #' + id + ' - ' + item.nome);
+                $form.find('[name="nome"]').val(item.nome);
+                $form.find('[name="cpf"]').val(item.cpf);
+                $form.find('[name="apartamento"]').val(item.apartamento);
+                $form.find('[name="bloco"]').val(item.bloco);
+                $form.prepend('<input type="hidden" name="id" value="'+item.id+'">');
+                $form.find('input').attr('disabled', false);
+            }
+        });
+    });
+} else {
+    $form.find('input').attr('disabled', false);
+}
+
+$form.on('submit', function(e) {
+    e.preventDefault();
+
+    let $name    = $form.find('[name="nome"]');
+    let $cpf     = $form.find('[name="cpf"]');
+    let $ap      = $form.find('[name="apartamento"]');
+    let $bloco   = $form.find('[name="bloco"]');
+
+    // console.log(id);
+    // return;
+    let $id;
+
+    if(id) {
+        $id = $form.find('[name="id"]');
+    }
+
+    // console.log($id.val());
+
+    // return;
+
+    $form.find('input').removeClass('is-invalid');
+    $form.find('.invalid-feedback').remove();
+
+    let formValidated = true;
+
+    if($name.val().length == 0) {
+        $name.addClass('is-invalid');
+        $name.parents('.form-group').append('<p class="invalid-feedback">Este campo é obrigatório</p>');
+        formValidated = false;
+    }
+
+    if($cpf.val().length == 0) {
+        $cpf.addClass('is-invalid');
+        $cpf.parents('.form-group').append('<p class="invalid-feedback">Este campo é obrigatório</p>');
+        formValidated = false;
+    }
+
+    if($ap.val().length == 0) {
+        $ap.addClass('is-invalid');
+        $ap.parents('.form-group').append('<p class="invalid-feedback">Este campo é obrigatório</p>');
+        formValidated = false;
+    }
+
+    if($bloco.val().length == 0) {
+        $bloco.addClass('is-invalid');
+        $bloco.parents('.form-group').append('<p class="invalid-feedback">Este campo é obrigatório</p>');
+        formValidated = false;
+    }
+
+    if(formValidated) {
+        $.ajax({
+            url: base_api + 'morador/salvar',
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "id":id ? $id.val() : null,
+                "nome":$name.val(),
+                "cpf":$cpf.val(),
+                "apartamento":$ap.val(),
+                "bloco":$bloco.val()
+            }),
+
+            success: function() {
+                alert('Morador ' + (id ? 'atualizado' : 'cadastrado') + ' com sucesso.');
+                window.location.href = base + 'moradores/';
+            },
+
+            error: function() {
+                alert('Erro na requisição.');
+                window.location.href = window.location.href;
+            }
+        });
+    }
+});
---
+++ controle-de-acesso-frontend-node/src/.components/table-acesso/_table-acesso.babel.js
@@ -0,0 +1,92 @@
+/**
+ * Cada componente recebe as variáveis explicadas abaixo
+ *
+ * @param {HTMLElement} el - Elemento DOM que seleciona o componente em si
+ * @param {Object} params  - Parâmetros para serem utilizados
+ */
+
+var datatables_ptbr = {
+    "sEmptyTable": "Nenhum registro encontrado",
+    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+    "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+    "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+    "sInfoPostFix": "",
+    "sInfoThousands": ".",
+    "sLengthMenu": "Mostrar _MENU_ resultados por página",
+    "sLoadingRecords": "Carregando...",
+    "sProcessing": "Processando...",
+    "sZeroRecords": "Nenhum registro encontrado",
+    "sSearch": "Pesquisar",
+    "oPaginate": {
+        "sNext": "Próximo",
+        "sPrevious": "Anterior",
+        "sFirst": "Primeiro",
+        "sLast": "Último"
+    },
+    "oAria": {
+        "sSortAscending": ": Ordenar colunas de forma ascendente",
+        "sSortDescending": ": Ordenar colunas de forma descendente"
+    }
+};
+
+$el             = $(el);
+
+const $table = $el.find('#items-table');
+
+$.get(base_api + 'acesso/pesquisar', function(items) {
+    $table.find('tbody').html('');
+    let $items = $(items);
+    $items.each(function(itemIndex, item) {
+        let item_html = '';
+        item_html = `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.dataCadastro}</td>
+                <td style="width:100px;">
+                    <a href="${base}controle-de-acesso/salvar/?id=${item.id}" class="btn btn-xs btn-block btn-warning"><i class="fas fa-edit"></i> Editar</a>
+                    <button type="button" class="btn btn-xs btn-block btn-danger" data-action="delete" data-id="${item.id}"><i class="fas fa-trash"></i> Exluir</a>
+                </td>
+            </tr>
+        `;
+
+        $table.find('tbody').append(item_html);
+    });
+
+    $table.DataTable({
+        oLanguage : datatables_ptbr,
+        order: [[0, "desc"]],
+        pageLength: 25,
+    });
+});
+
+$(document).on('click', '[data-action]', function(e) {
+    e.preventDefault();
+    let $btn = $(this);
+    let action = $btn.attr('data-action');
+    let id = $btn.attr('data-id');
+
+    switch(action) {
+        case 'delete':
+            let confirm = window.confirm('Tem certeza que deseja apagar este acesso?');
+
+            if(confirm) {
+                $.ajax({
+                    url: base_api + 'acesso/apagar/?id=' + id,
+                    type: 'DELETE',
+                    success: function(response, textStatus, xhr) {
+                        if(textStatus == 'success') {
+                            alert('Acesso excluído com sucesso.');
+                            window.location.href = window.location.href;
+                        } else {
+                            alert('Erro na requisição.');
+                        }
+                    }
+                });
+            }
+        break;
+
+        default:
+            alert('No action defined.');
+        break;
+    }
+});
