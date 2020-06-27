57508559bc31f8d46cc2de5da806dcbbec2ede50
Gabriel Leite <gabrielleite@luego.com.br>
2020-06-27 13:25:13 -0300

    Publicando no GitHub

--- controle-de-acesso-frontend-node/src/.components/form-salvar-entregador/_form-salvar-entregador.babel.js
+++ controle-de-acesso-frontend-node/src/.components/form-salvar-entregador/_form-salvar-entregador.babel.js
@@ -1,8 +1,8 @@
 /**
- * O componente recebe as variáveis explicadas abaixo
+ * Cada componente recebe as variáveis explicadas abaixo
  *
  * @param {HTMLElement} el - Elemento DOM que seleciona o componente em si
  * @param {Object} params  - Parâmetros para serem utilizados
  */
 
-// $el             = $(el);
+const $el = $(el);
